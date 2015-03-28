aef299b06bc2541e256b2a52613276a3b61ab382
thopras <thopras@tpl-win7-x64.localdomain>
2015-03-28 10:39:35 +0100

    - Configuration    Ensure that android device can connect to wamp server inside vmplayer - see readme-project-config.txt  - app crashes after response    because substring(0,500) called on string which is shorter than 500 chars - added logging

--- app/src/main/java/itmelange/com/mytest2go/MyConnectActivity.java
+++ app/src/main/java/itmelange/com/mytest2go/MyConnectActivity.java
@@ -2,6 +2,7 @@ package itmelange.com.mytest2go;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +13,10 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
 
 import itmelange.com.mytest2go.model.MyAppSingleton;
 
@@ -58,14 +62,44 @@ public class MyConnectActivity extends ActionBarActivity {
      * ======================================================================
      */
 
+    public void sendMessage_json(View view) {
+
+        EditText editPosition = (EditText) findViewById(R.id.editPosition);
+        final TextView txtRespondMsg = (TextView) findViewById(R.id.txtRespondMsg);
+
+        String posText = editPosition.getText().toString();
+
+        // get request queue and send request
+        String url ="http://192.168.43.100:8090/test/testservice.php";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        handleResponse(txtRespondMsg, response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        handleResponseError(txtRespondMsg, error);
+                    }
+                });
+
+    }
+
     /**
      * send message button clicked
      * @param view
      */
-    public void sendMessage(View view) {
+    public void sendMessage_String(View view) {
+
         //
         EditText editPosition = (EditText) findViewById(R.id.editPosition);
         final TextView txtRespondMsg = (TextView) findViewById(R.id.txtRespondMsg);
+
+        txtRespondMsg.setText("send request...");
 
         String posText = editPosition.getText().toString();
 
@@ -77,19 +111,58 @@ public class MyConnectActivity extends ActionBarActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        txtRespondMsg.setText("Response is: "+ response.substring(0,500));
+                        handleResponse(txtRespondMsg, response);
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                String errormsg = "That didn't work!" + error.getMessage() + error.getStackTrace() + error.getCause();
-                txtRespondMsg.setText(errormsg);
-            }
-        });
-
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        handleResponseError(txtRespondMsg, error);
+                    }
+                });
 
         RequestQueue requestQueue = MyAppSingleton.getInstance(this.getApplicationContext()).getRequestQueue();
         MyAppSingleton.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+
+    /**
+     * Handle response method
+     * @param txtRespondMsg  the view
+     * @param response the response string
+     */
+    private void handleResponse(TextView txtRespondMsg, String response) {
+        try {
+            if (txtRespondMsg == null) {
+                return;
+            }
+
+            if (response != null) {
+                // Display the response string.
+                txtRespondMsg.setText("Response is: " + response);
+            } else {
+                txtRespondMsg.setText("response is null");
+            }
+
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG) {
+                Log.e("responseZugriff", "Fehler beim Response-Zugriff");
+                Log.e("responseZugriff", e.toString());
+                Log.e("responseZugriff", e.getMessage());
+                Log.e("responseZugriff", e.getStackTrace().toString());
+                Log.e("responseZugriff", e.getCause().toString());
+            }
+
+        }
+    }
+
+    /**
+     * The error handle method
+     * @param txtRespondMsg  the view
+     * @param error volley error
+     */
+    private void handleResponseError(TextView txtRespondMsg, VolleyError error) {
+
+        String errormsg = "That didn't work!" + error.getMessage() + error.getStackTrace() + error.getCause();
+        txtRespondMsg.setText(errormsg);
     }
 }
